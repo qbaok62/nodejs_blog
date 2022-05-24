1d2687e6b519085812bfc6fceeb1465435d1e3cb
qbaok62 <qbaok62@gmail.com>
2022-05-24 22:12:38 +0700

    finish

--- src/app/models/Course.js
+++ src/app/models/Course.js
@@ -1,12 +1,33 @@
 const mongoose = require('mongoose');
+const slug = require('mongoose-slug-generator');
+const mongoose_delete = require('mongoose-delete');
+const AutoIncrement = require('mongoose-sequence')(mongoose);
+
 const Schema = mongoose.Schema;
 
-const Course = new Schema({
-    name: { type: String, maxlength: 255 },
-    description: { type: String, maxlength: 600 },
-    image: { type: String, maxlength: 255 },
-    createdAt: { type: Date, default: Date.now },
-    updatedAt: { type: Date, default: Date.now },
+const Course = new Schema(
+    {
+        _id: { type: Number },
+        name: { type: String, required: true },
+        description: { type: String },
+        image: { type: String },
+        videoId: { type: String, required: true },
+        level: { type: String },
+        slug: { type: String, slug: 'name', unique: true },
+    },
+    {
+        _id: false, 
+        timestamps: true 
+    },
+);
+
+//Add plugins
+mongoose.plugin(slug);
+
+Course.plugin(AutoIncrement);
+Course.plugin(mongoose_delete, {
+    deletedAt : true , 
+    overrideMethods: 'all' 
 });
 
 module.exports = mongoose.model('Course', Course);
